6b2d6f721678fd036f9cd67a9c6f096170650045
eldar <eldar.ablaev@gmail.com>
2013-06-20 01:53:23 +0300

    generate EldarBlogBundle

---
+++ src/Eldar/BlogBundle/Controller/DefaultController.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace Eldar\BlogBundle\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+
+class DefaultController extends Controller
+{
+    public function indexAction($name)
+    {
+        return $this->render('EldarBlogBundle:Default:index.html.twig', array('name' => $name));
+    }
+}
---
+++ src/Eldar/BlogBundle/EldarBlogBundle.php
@@ -0,0 +1,9 @@
+<?php
+
+namespace Eldar\BlogBundle;
+
+use Symfony\Component\HttpKernel\Bundle\Bundle;
+
+class EldarBlogBundle extends Bundle
+{
+}
